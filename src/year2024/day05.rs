87c92aeaefba38fc81a60773ec0fcf648f595182
BigWalrus <morgjik@gmail.com>
2025-12-02 08:08:12 +0100

    some other puzzles

--- src/year2024/day05.rs
+++ src/year2024/day05.rs
@@ -1,7 +1,7 @@
 use std::{fs, str::FromStr};
 
 pub fn solution_part1() -> i32 {
-    let input = fs::read_to_string("src/year2024/data/input_day03").expect("couldn't read a file");
+    let input = fs::read_to_string("src/year2024/data/input_day05").expect("couldn't read a file");
     let lines: Vec<&str> = input.trim().split("\n").collect();
     let mut out = 0;
 
@@ -9,7 +9,7 @@ pub fn solution_part1() -> i32 {
 }
 
 pub fn solution_part2() -> i32 {
-    let input = fs::read_to_string("src/year2024/data/input_day03").expect("couldn't read a file");
+    let input = fs::read_to_string("src/year2024/data/input_day05").expect("couldn't read a file");
     let lines: Vec<&str> = input.trim().split("\n").collect();
     let mut out = 0;
 
